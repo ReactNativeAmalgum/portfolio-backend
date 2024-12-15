1e2e7baf344215eb92976d7f1a9147e42481c8d8
reactnativeamalgum <reactnativeamalgum@gmail.com>
2024-12-15 15:58:41 +0530

    cors

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.use('/api/send-email', SendEmail); //remove because vercel has its own /api
+// app.use('/api/send-email', SendEmail); //remove because vercel has its own /api
+
+app.post('/api/send-email', (req, res) => {
+  const { name, email, message } = req.body;
+  console.log('Email data:', { name, email, message });
+  res.status(200).json({ success: true, message: 'Email sent!' });
+});
 
 const PORT = process.env.PORT || 5000;
 
